451e3119fe5a121c418a1afa72960c14d79279d0
CBIS2022Gayanthika <123550757+CBIS2022Gayanthika@users.noreply.github.com>
2023-03-22 11:56:12 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+input.onGesture(Gesture.Shake, function () {
+    crying.showImage(0)
+    basic.pause(5000)
+})
+let crying: Image = null
+let happy = images.createImage(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+crying = images.createImage(`
+    . # . # .
+    # . . . #
+    # . . . #
+    . # # # .
+    # . . . #
+    `)
+let sad = images.createImage(`
+    . . . . .
+    . # . # .
+    . . . . .
+    . # # # .
+    # . . . #
+    `)
+basic.forever(function () {
+	
+})
